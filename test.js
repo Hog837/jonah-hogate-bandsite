2a364f183e7cabfaea79153b55bbf69e46b51d5d
Jonah Hogate <Jonah.hogate@cox.net>
2021-08-04 23:58:14 -0500

    created js for shows

---
+++ test.js
@@ -0,0 +1,18 @@
+const shows = [
+    {
+        date: 'blah',
+        venue: 'blah',
+        location: {
+            city: 'blah',
+            state: 'blah'
+        }
+    }, {
+        date: 'blah',
+        venue: 'blah',
+        location: {
+            city: 'blah',
+            state: 'blah'
+        }
+    }
+];
+
