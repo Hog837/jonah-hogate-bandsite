3496a6dc074703aa349290c4041a5484a1db641b
Jonah Hogate <Jonah.hogate@cox.net>
2021-08-07 13:14:45 -0500

    changed JS for shows list

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,61 +1,101 @@
-const shows = [
+let shows = [
     {
         date: 'Mon Sept 06 2021',
         venue: 'Ronald Lane',
         location: 'San Francisco, CA'
-    }, {
+    },
+    {
         date: 'Tue Sept 21 2021',
         venue: 'Pier 3 East',
         location: 'San Francisco, CA'
-    }, {
+    },
+    {
         date: 'Fri Oct 15 2021',
         venue: 'View Lounge',
         location: 'San Francisco, CA'
-    }, {
+    },
+    {
         date: 'Sat Nov 06 2021',
         venue: 'Hyatt Agency',
         location: 'San Francisco, CA'
-    },{
-        date: 'Sat Nov 26 2021',
+    },
+    {
+        date: 'Fri Nov 26 2021',
         venue: 'Moscow Center',
         location: 'San Francisco, CA'
-    }, {
+    },
+    {
         date: 'Wed Dec 15 2021',
         venue: 'Press Club',
         location: 'San Francisco, CA'
-    }
+    },
 ];
 
-console.log('hello')
+const showsList = document.querySelector('.shows__list');
 
-let main = document.querySelector('main')
+const generateShowsContent = (showData) => {
 
-let section = document.createElement('section__shows');
+/*created section for shows*/
 
-for (let i = 0; i < shows.length; i++){
-    let row = document.createElement('div');   
+const showListSection = document.createElement('section');
+showListSection.classList.add('shows__section');
+showsList.appendChild(showListSection);
 
-    let p1 = document.createElement('P');
-    let p2 = document.createElement('p');
-    let p3 = document.createElement('p');
-    let p4 = document.createElement('p');
+/*create dates section*/
 
-    let text1 = document.createTextNode(shows[i].date);
-    let text2 = document.createTextNode(shows[i].venue);
-    let text3 = document.createTextNode(`${shows[i].location.city}, ${shows[i].location.state}`);
-    let button = document.createElement('button')
-    let buttonContent = document.createTextNode('Buy Tickets');
-    button.appendChild(buttonContent);
+const datesHeader = document.createElement('span');
+datesHeader.classList.add('shows__header');
+datesHeader.innerText = 'DATES';
+showListSection.appendChild(datesHeader);
 
-    p1.appendChild(text1);
-    p2.appendChild(text2);
-    p3.appendChild(text3);
-    row.appendChild(p1);
-    row.appendChild(p2);
-    row.appendChild(p3);
-    row.appendChild(button);
+const datesData = document.createElement('p');
+datesData.classList.add('shows__date');
+datesData.innerText = showData.date;
+showListSection.appendChild(datesData);
 
-    section.appendChild(row);
+/*creates venue section*/
+
+const venueHeader = document.createElement('span');
+venueHeader.classList.add('shows__header');
+venueHeader.innerText = 'VENUE';
+showListSection.appendChild(venueHeader);
+
+const venueData = document.createElement('p');
+venueData.classList.add('shows__data');
+venueData.innerText = showData.venue;
+showListSection.appendChild(venueData);
+
+/*create location section*/
+
+const locationHeader = document.createElement('span');
+locationHeader.classList.add('shows__header');
+locationHeader.innerText = 'LOCATION';
+showListSection.appendChild(locationHeader);
+
+const locationData = document.createElement('p');
+locationData.classList.add('shows__data');
+locationData.innerText = showData.location;
+showListSection.appendChild(locationData);
+
+const buyBtn = document.createElement('button');
+buyBtn.classList.add('shows__button');
+buyBtn.innerText = 'BUY TICKETS';
+showListSection.appendChild(buyBtn);
+
+const divider = document.createElement('hr');
+divider.classList.add('shows__divider');
+showListSection.appendChild(divider);
+
+return showListSection
+
 }
-
-main.appendChild(section);
+const generateShowsList = (shows) => {
+    for (let i = 0; i < shows.length; i++){
+        const showData = shows[i];
+        console.log('Shows Data: ', showData);
+        const showSections = generateShowsContent(showData);
+        console.log('Shows Sections: ', showSections);
+        showsList.appendChild(showSections);
+    }
+}
+generateShowsList(shows);
